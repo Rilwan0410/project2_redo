fbbdd0da413f29247fc87a5681f36079a11049d1
User <user@Users-MacBook-Air.local>
2024-02-10 06:26:03 -0500

    added tailwind

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const listingRoutes = require('./listingRoutes');
-
-router.use('/users', userRoutes);
-router.use('/Listings', listingRoutes);
-
-module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 // Update the require statement to 'homeRoutes' instead of 'homeController'
 const homeRoutes = require('./homeRoutes');
 
-const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
