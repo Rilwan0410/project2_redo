bd6db239f060eca31f3185dbacbce662f13f3e09
User <user@Users-MacBook-Air.local>
2024-02-13 04:49:58 -0500

    removed validate constraint from User model for password -- length can be over 8 characters

--- models/User.js
+++ models/User.js
@@ -38,23 +38,10 @@ User.init(
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
+      allowNull: false
     },
   },
   {
-    // hooks: {
-    //   beforeCreate: async (newUserData) => {
-    //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
-    //     return newUserData;
-    //   },
-    //   beforeUpdate: async (updatedUserData) => {
-    //     updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-    //     return updatedUserData;
-    //   },
-    // },
     sequelize,
     timestamps: false,
     freezeTableName: true,
